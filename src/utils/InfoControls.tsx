04c739294f97cf1daf3598df462e92d66bfb4a35
Emmanuel Krebs <e-krebs@users.noreply.github.com>
2022-05-22 20:22:37 +0200

    inline svg

--- src/utils/InfoControls.tsx
+++ src/utils/InfoControls.tsx
@@ -1,7 +1,7 @@
 import { Controls } from '@ladle/react/lib/app/src/icons';
 
 export const InfoControls = () => (
-  <p className="flex items-center space-x-2 pl-2 border-l-4 border-blue-500">
+  <p className="space-x-2 -space-y-px pl-2 border-l-4 border-blue-500">
     <span>You can change the input parameters using the controls icon below</span>
     <Controls />
   </p>
