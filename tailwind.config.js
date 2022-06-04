866d72eb27791610ee0521d439cb1bb10c4f6296
Emmanuel Krebs <e-krebs@users.noreply.github.com>
2022-06-04 18:15:07 +0200

    add a header

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ const shared = require('./shared.tailwind.config');
 /** @type {import("@types/tailwindcss/tailwind-config").TailwindConfig } */
 module.exports = {
   presets: [shared],
-  content: ["./{src,.ladle}/**/*.{ts,tsx}"],
-  safelist: ["ladle-addons", 'ladle-wrapper', 'ladle-controls-table', 'ladle-aside'],
+  content: ['./{src,.ladle}/**/*.{ts,tsx}'],
+  safelist: [
+    'ladle-wrapper',
+    'ladle-main',
+    'ladle-aside',
+    'ladle-addons',
+    'ladle-controls-table'
+  ],
 };
