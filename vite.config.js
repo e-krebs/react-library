5deca5f20cfd739522e4e4f86fbf2994e56876e3
Emmanuel Krebs <e-krebs@users.noreply.github.com>
2022-07-16 12:42:31 +0200

    chore: upgrade ladle for windows compatibilty (#17)
    closes #15 
    
    - general dependencies upgrade (incl. ladle)
    - add some workaround for path resolution issues

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,16 @@
-export default {
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+const projectRootDir = resolve(__dirname);
+
+export default defineConfig({
   server: {
     open: false,
   },
-};
+  resolve: {
+    alias: {
+      '@react-aria': resolve(projectRootDir, 'node_modules/@react-aria'),
+      '@react-stately': resolve(projectRootDir, 'node_modules/@react-stately'),
+    },
+  },
+});
