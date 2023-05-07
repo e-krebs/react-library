8cdaf95c20ef1a03b2a935ba45b1584a2d757b16
Emmanuel Krebs <e-krebs@users.noreply.github.com>
2023-05-07 16:03:27 +0200

    feat: use useModalContext to bind onPress with closeModal

--- src/Modal/ModalCloseButton.tsx
+++ src/Modal/ModalCloseButton.tsx
@@ -1,11 +1,16 @@
 import { FC } from 'react';
 
 import { Button, ButtonProps } from '../Button';
+import { useModalContext } from './useModal';
 
-type ModalCloseButton = Omit<ButtonProps, 'type' | 'formMethod'>;
+type ModalCloseButton = Omit<ButtonProps, 'type' | 'formMethod' | 'onPress'>;
 
-export const ModalCloseButton: FC<ModalCloseButton> = ({ children, ...props }) => (
-  <Button type="submit" formMethod="dialog" {...props}>
-    {children}
-  </Button>
-);
+export const ModalCloseButton: FC<ModalCloseButton> = ({ children, ...props }) => {
+  const { closeModal } = useModalContext();
+
+  return (
+    <Button type="submit" formMethod="dialog" onPress={closeModal} {...props}>
+      {children}
+    </Button>
+  );
+};
