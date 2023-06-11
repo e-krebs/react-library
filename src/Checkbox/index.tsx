1760081e4aa824073963e688a9eed7bd05c9bc2a
Emmanuel Krebs <e-krebs@users.noreply.github.com>
2023-06-11 17:44:51 +0200

    feat: migrate checkbox

--- src/Checkbox/index.tsx
+++ src/Checkbox/index.tsx
@@ -1,36 +1,30 @@
 import cx from 'classnames';
-import { AriaCheckboxProps } from '@react-types/checkbox';
-import { FC, useMemo, useRef } from 'react';
-import { VisuallyHidden } from '@react-aria/visually-hidden';
-import { useFocusRing } from '@react-aria/focus';
-import { useCheckbox } from '@react-aria/checkbox';
-import { useToggleState } from '@react-stately/toggle';
-import { CheckSquare, Icon, Square } from 'react-feather';
+import type { FC, RefAttributes } from 'react';
+import {
+  Checkbox as AriaCheckBox,
+  type CheckboxProps as AriaCheckboxProps,
+} from 'react-aria-components';
+import { CheckSquare, Square } from 'react-feather';
 
-export interface CheckboxProps extends Omit<AriaCheckboxProps, 'children'> {
+export interface CheckboxProps
+  extends Omit<AriaCheckboxProps, 'children'>,
+    RefAttributes<HTMLInputElement> {
   label?: string;
+  className?: string;
 }
 
-export const Checkbox: FC<CheckboxProps> = ({ label, ...props }) => {
-  const state = useToggleState(props);
-  const ref = useRef(null);
-  const { inputProps } = useCheckbox({ ...props, 'aria-label': label }, state, ref);
-  const { focusProps } = useFocusRing();
-
-  const CheckIcon: Icon = useMemo(() => (state.isSelected ? CheckSquare : Square), [state]);
-
-  return (
-    <label
-      className={cx(
-        'flex w-fit items-center gap-x-2',
-        props.isDisabled && 'cursor-not-allowed opacity-40'
-      )}
-    >
-      <VisuallyHidden>
-        <input {...inputProps} {...focusProps} aria-label={label} ref={ref} />
-      </VisuallyHidden>
-      <CheckIcon />
-      {label && <span>{label}</span>}
-    </label>
-  );
-};
+export const Checkbox: FC<CheckboxProps> = ({ label, className, ...props }) => (
+  <AriaCheckBox
+    {...props}
+    className={({ isDisabled }) =>
+      cx('flex w-fit items-center gap-x-2', isDisabled && 'cursor-not-allowed opacity-40', className)
+    }
+  >
+    {({ isSelected }) => (
+      <>
+        {isSelected ? <CheckSquare /> : <Square />}
+        {label}
+      </>
+    )}
+  </AriaCheckBox>
+);
