90a6f5c1cedc1276df1cdf7b3e56049aefeab48b
Emmanuel Krebs <e-krebs@users.noreply.github.com>
2023-08-04 16:14:12 +0200

    feat: don't use shadow dom

--- .ladle/components.tsx
+++ .ladle/components.tsx
@@ -1,28 +1,23 @@
-import type { GlobalProvider } from "@ladle/react";
+import type { GlobalProvider } from '@ladle/react';
 import React from 'react';
 import { GitHub } from 'react-feather';
-import shadow from 'react-shadow';
 
-
-import tailwindCss from '../src/tailwind.css?inline';
+import '../src/tailwind.css';
 import './stories.css';
 
 export const Provider: GlobalProvider = ({ children }) => (
   <>
-    <header className="flex justify-between w-full mx-auto">
+    <header className="mx-auto flex w-full justify-between">
       <h1>React library</h1>
       <a
         href="https://github.com/e-krebs/react-library"
         target="_blank"
         title="GitHub repository"
-        className="flex justify-center items-center mt-1.5 w-10 h-10"
+        className="mt-1.5 flex h-10 w-10 items-center justify-center"
       >
-        <GitHub className="w-6 h-6" />
+        <GitHub className="h-6 w-6" />
       </a>
     </header>
-    <shadow.section>
-      <style type="text/css">{tailwindCss}</style>
-      {children}
-    </shadow.section>
+    <section>{children}</section>
   </>
 );
