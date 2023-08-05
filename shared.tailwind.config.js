7211a5f7a12b3ed25559c5f66a91a1f1c8abea38
Emmanuel Krebs <e-krebs@users.noreply.github.com>
2023-08-05 14:09:14 +0200

    feat: define primary color for theming

--- shared.tailwind.config.js
+++ shared.tailwind.config.js
@@ -1,12 +1,27 @@
 const colors = require('tailwindcss/colors');
 const typography = require('@tailwindcss/typography');
 
+const primary = {
+  '50':  `var(--primary-50,  ${colors.zinc[50]})`,
+  '100': `var(--primary-100, ${colors.zinc[100]})`,
+  '200': `var(--primary-200, ${colors.zinc[200]})`,
+  '300': `var(--primary-300, ${colors.zinc[300]})`,
+  '400': `var(--primary-400, ${colors.zinc[400]})`,
+  '500': `var(--primary-500, ${colors.zinc[500]})`,
+  '600': `var(--primary-600, ${colors.zinc[600]})`,
+  '700': `var(--primary-700, ${colors.zinc[700]})`,
+  '800': `var(--primary-800, ${colors.zinc[800]})`,
+  '900': `var(--primary-900, ${colors.zinc[900]})`,
+  '950': `var(--primary-950, ${colors.zinc[950]})`,
+};
+
 module.exports = {
   content: ["./src/**/*.{ts,tsx}"],
   theme: {
     extend: {
       colors: {
         'gray': colors.zinc,
+        primary,
       },
       keyframes: {
         'modal-grow': {
