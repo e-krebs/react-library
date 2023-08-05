8f6d2823fe3203b6e633b74dedcac0fb1479e7e7
Emmanuel Krebs <e-krebs@users.noreply.github.com>
2023-08-05 21:33:58 +0200

    fix: (tentative) iPad & transitions + color ring + bg on div vs. svg

--- src/Checkbox/index.tsx
+++ src/Checkbox/index.tsx
@@ -24,23 +24,25 @@ export const Checkbox: FC<CheckboxProps> = ({ label, className, ...props }) => (
   >
     <div
       className={`
-        flex items-center justify-center m-0.5 p-0 h-5 w-5
+        flex items-center justify-center h-5 w-5
         border-2 rounded border-th-border dark:border-th-dark-border
         group-data-selected/checkbox:border-th-primary dark:group-data-selected/checkbox:border-th-dark-primary
+        group-data-selected/checkbox:bg-th-primary dark:group-data-selected/checkbox:bg-th-dark-primary
         group-data-focused/checkbox:ring-2 ring-th-border dark:ring-th-dark-border
+        group-data-selected/checkbox:ring-th-primary group-data-selected/checkbox:dark:ring-th-dark-primary
         ring-offset-2 ring-offset-th-bg dark:ring-offset-th-dark-bg
-        transition motion-reduce:transition-none
+        transition-all duration-300 motion-reduce:transition-none
       `}
     >
       <svg
         viewBox="0 0 18 18"
         aria-hidden="true"
         className={`
-          fill-none
-          group-data-selected/checkbox:bg-th-primary dark:group-data-selected/checkbox:bg-th-dark-primary
-          stroke-3 stroke-transparent
-          group-data-selected/checkbox:stroke-th-bg dark:group-data-selected/checkbox:stroke-th-border
-          transition motion-reduce:transition-none
+          w-4 h-4 fill-none
+          stroke-3 stroke-th-bg dark:stroke-th-dark-bg
+          [stroke-dasharray:22px]
+          [stroke-dashoffset:66] group-data-selected/checkbox:[stroke-dashoffset:44]
+          transition-all duration-300 motion-reduce:transition-none
         `}
       >
         <polyline points="1 9 7 14 15 4" />
