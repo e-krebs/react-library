13233a38342cf64f9ab4a57475eb82ba7037c28c
Emmanuel Krebs <e-krebs@users.noreply.github.com>
2023-09-03 11:26:05 +0200

    fix: lintintg

--- shared.tailwind.config.js
+++ shared.tailwind.config.js
@@ -3,25 +3,25 @@ const colors = require('tailwindcss/colors');
 const typography = require('@tailwindcss/typography');
 
 const primary = {
-  '50':  `var(--primary-50,  ${colors.zinc[50]})`,
-  '100': `var(--primary-100, ${colors.zinc[100]})`,
-  '200': `var(--primary-200, ${colors.zinc[200]})`,
-  '300': `var(--primary-300, ${colors.zinc[300]})`,
-  '400': `var(--primary-400, ${colors.zinc[400]})`,
-  '500': `var(--primary-500, ${colors.zinc[500]})`,
-  '600': `var(--primary-600, ${colors.zinc[600]})`,
-  '700': `var(--primary-700, ${colors.zinc[700]})`,
-  '800': `var(--primary-800, ${colors.zinc[800]})`,
-  '900': `var(--primary-900, ${colors.zinc[900]})`,
-  '950': `var(--primary-950, ${colors.zinc[950]})`,
+  50: `var(--primary-50,  ${colors.zinc[50]})`,
+  100: `var(--primary-100, ${colors.zinc[100]})`,
+  200: `var(--primary-200, ${colors.zinc[200]})`,
+  300: `var(--primary-300, ${colors.zinc[300]})`,
+  400: `var(--primary-400, ${colors.zinc[400]})`,
+  500: `var(--primary-500, ${colors.zinc[500]})`,
+  600: `var(--primary-600, ${colors.zinc[600]})`,
+  700: `var(--primary-700, ${colors.zinc[700]})`,
+  800: `var(--primary-800, ${colors.zinc[800]})`,
+  900: `var(--primary-900, ${colors.zinc[900]})`,
+  950: `var(--primary-950, ${colors.zinc[950]})`,
 };
 
 module.exports = {
-  content: ["./src/**/*.{ts,tsx}"],
+  content: ['./src/**/*.{ts,tsx}'],
   theme: {
     extend: {
       colors: {
-        'gray': colors.zinc,
+        gray: colors.zinc,
         primary,
         'th-primary': primary[500],
         'th-dark-primary': primary[300],
@@ -41,19 +41,19 @@ module.exports = {
         'th-dark': colors.zinc[900],
       },
       textColor: {
-        'th': colors.zinc[500],
+        th: colors.zinc[500],
         'th-dark': colors.zinc[400],
       },
       fill: {
-        'th': colors.zinc[500],
+        th: colors.zinc[500],
         'th-dark': colors.zinc[400],
       },
       borderColor: {
-        'th': colors.zinc[700],
+        th: colors.zinc[700],
         'th-dark': colors.zinc[300],
       },
       ringColor: {
-        'th': colors.zinc[700],
+        th: colors.zinc[700],
         'th-dark': colors.zinc[300],
       },
       spacing: {
@@ -61,7 +61,7 @@ module.exports = {
         input: spacing[9],
       },
       strokeWidth: {
-        '3': '3px',
+        3: '3px',
       },
       opacity: {
         disabled: opacity[40],
@@ -69,24 +69,24 @@ module.exports = {
       keyframes: {
         'modal-grow': {
           '0%': { transform: 'scale(0)' },
-          '100%': { transform: 'scale(1)' }
+          '100%': { transform: 'scale(1)' },
         },
         'modal-shrink': {
           '0%': { transform: 'scale(1)', opacity: 1 },
-          '100%': { transform: 'scale(0)', opacity: 0 }
-        }
+          '100%': { transform: 'scale(0)', opacity: 0 },
+        },
       },
       animation: {
         'modal-grow': 'modal-grow 150ms cubic-bezier(0.4, 0, 0.2, 1)',
-        'modal-shrink': 'modal-shrink 150ms cubic-bezier(0.4, 0, 0.2, 1)'
-      }
+        'modal-shrink': 'modal-shrink 150ms cubic-bezier(0.4, 0, 0.2, 1)',
+      },
     },
     data: {
       disabled: 'disabled',
       focused: 'focused',
       'focus-visible': 'focus-visible',
       selected: 'selected',
-    }
+    },
   },
   plugins: [typography],
 };
