c045d250489b88523c75082e1ece18023989a1d3
Emmanuel Krebs <e-krebs@users.noreply.github.com>
2023-09-03 16:04:57 +0200

    fix: lint & use theme values
    compatibility with opacity

--- shared.tailwind.config.js
+++ shared.tailwind.config.js
@@ -2,29 +2,27 @@ const { opacity, spacing } = require('tailwindcss/defaultTheme');
 const colors = require('tailwindcss/colors');
 const typography = require('@tailwindcss/typography');
 
-const primary = {
-  50: `var(--primary-50,  ${colors.zinc[50]})`,
-  100: `var(--primary-100, ${colors.zinc[100]})`,
-  200: `var(--primary-200, ${colors.zinc[200]})`,
-  300: `var(--primary-300, ${colors.zinc[300]})`,
-  400: `var(--primary-400, ${colors.zinc[400]})`,
-  500: `var(--primary-500, ${colors.zinc[500]})`,
-  600: `var(--primary-600, ${colors.zinc[600]})`,
-  700: `var(--primary-700, ${colors.zinc[700]})`,
-  800: `var(--primary-800, ${colors.zinc[800]})`,
-  900: `var(--primary-900, ${colors.zinc[900]})`,
-  950: `var(--primary-950, ${colors.zinc[950]})`,
-};
-
 module.exports = {
   content: ['./src/**/*.{ts,tsx}'],
   theme: {
     extend: {
       colors: {
         gray: colors.zinc,
-        primary,
-        'th-primary': primary[500],
-        'th-dark-primary': primary[300],
+        primary: {
+          50: `rgb(var(--primary-50,  ${colors.zinc[50]}) / <alpha-value>)`,
+          100: `rgb(var(--primary-100, ${colors.zinc[100]}) / <alpha-value>)`,
+          200: `rgb(var(--primary-200, ${colors.zinc[200]}) / <alpha-value>)`,
+          300: `rgb(var(--primary-300, ${colors.zinc[300]}) / <alpha-value>)`,
+          400: `rgb(var(--primary-400, ${colors.zinc[400]}) / <alpha-value>)`,
+          500: `rgb(var(--primary-500, ${colors.zinc[500]}) / <alpha-value>)`,
+          600: `rgb(var(--primary-600, ${colors.zinc[600]}) / <alpha-value>)`,
+          700: `rgb(var(--primary-700, ${colors.zinc[700]}) / <alpha-value>)`,
+          800: `rgb(var(--primary-800, ${colors.zinc[800]}) / <alpha-value>)`,
+          900: `rgb(var(--primary-900, ${colors.zinc[900]}) / <alpha-value>)`,
+          950: `rgb(var(--primary-950, ${colors.zinc[950]}) / <alpha-value>)`,
+        },
+        'th-primary': `rgb(var(--primary-500, ${colors.zinc[500]}) / <alpha-value>)`,
+        'th-dark-primary': `rgb(var(--primary-400, ${colors.zinc[400]}) / <alpha-value>)`,
       },
       backgroundColor: {
         th: colors.white,
