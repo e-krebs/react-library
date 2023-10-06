f5b6bcdc86a424e32ebfdd2f138ecf2ee9939fa0
Emmanuel Krebs <e-krebs@users.noreply.github.com>
2023-10-06 13:00:20 +0200

    fix: clean vite config

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,3 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
 
-const projectRootDir = resolve(__dirname);
-
-export default defineConfig({
-  server: {
-    open: false,
-  },
-  resolve: {
-    alias: {
-      '@react-aria': resolve(projectRootDir, 'node_modules/@react-aria'),
-      classnames: resolve(projectRootDir, 'node_modules/classnames'),
-      react: resolve(projectRootDir, 'node_modules/react'),
-      'react-feather': resolve(projectRootDir, 'node_modules/react-feather'),
-      'react-aria-components': resolve(projectRootDir, 'node_modules/react-aria-components'),
-    },
-  },
-});
+export default defineConfig({ server: { open: false } });
