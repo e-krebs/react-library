8d1e2de03c09c3c23af9cf51df205f8a5e8f47f2
Emmanuel Krebs <e-krebs@users.noreply.github.com>
2024-12-16 10:32:46 +0100

    feat: input width (#85)

--- src/types.ts
+++ src/types.ts
@@ -2,5 +2,6 @@ import { FC, SVGAttributes } from 'react';
 
 export type InputBorder = 'none' | 'bottom' | 'rounded';
 export type InputFlow = 'col' | 'row';
+export type InputWidth = 'xs' | 's' | 'm' | 'l' | 'xl';
 
 export type IconComponent = FC<SVGAttributes<SVGElement>>;
